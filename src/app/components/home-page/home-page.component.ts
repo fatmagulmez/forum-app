80e59e3d34fd32d2d60ce47fa6f85d2b1b89087c
fatmagulmez <ffatma.glmz@gmail.com>
2024-02-29 23:35:01 +0300

    detail-page added

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,12 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { CategoryTableComponent } from '../../shared/category-table/category-table.component';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [CategoryTableComponent, CommonModule],
+  imports: [ CommonModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
--- src/app/components/topic-page/topic-page.component.ts
+++ src/app/components/topic-page/topic-page.component.ts
@@ -1,29 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { CategoryTable } from '../../interfaces/category-table';
-import { CategoryTableComponent } from '../../shared/category-table/category-table.component';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-topic-page',
   standalone: true,
-  imports: [CommonModule, CategoryTableComponent],
+  imports: [CommonModule],
   templateUrl: './topic-page.component.html',
   styleUrl: './topic-page.component.scss'
 })
 export class TopicPageComponent implements OnInit{
-
-  topicCategoryTableConfig: CategoryTable;
-  dataList = [
-    {
-      headerTitle: 'Konu',
-      count: 6
-    },
-    {
-      headerTitle: 'Konu',
-      count: 6
-    }
-  ]
 
   constructor(private router : Router) {
 
